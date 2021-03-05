c4ca2c4f70d7f292f27aa3dbea90c5326b9ddf8e
labradorGitHubCanine <123349527@qq.com>
2021-03-05 17:34:36 +0800

    0

--- vue.config.js
+++ vue.config.js
@@ -8,17 +8,56 @@ module.exports = {
         config.plugins.delete('prefetch')
     },
     productionSourceMap: false,
-    configureWebpack: {
-        externals: {
-            'vue': 'Vue',
-            'vue-router': 'VueRouter',
-            'axios': 'axios',
-            'element-ui': 'ElementUI',
-            'lottie': 'lottie',
-            'introJs': 'introJs',
-            'highcharts': 'highcharts',
-            'highcharts-vue': 'highcharts-vue'
+    // configureWebpack:  {
+    //     externals: {
+    //         'vue': 'Vue',
+    //         'vue-router': 'VueRouter',
+    //         'axios': 'axios',
+    //         'element-ui': 'ElementUI',
+    //         'lottie': 'lottie',
+    //         'introJs': 'introJs',
+    //         'highcharts': 'highcharts',
+    //         'highcharts-vue': 'highcharts-vue'
+    //     }
+    // },
+    configureWebpack: config => {
+        // 生产环境配置
+        if (process.env.NODE_ENV === 'production') {
+
+            // 消除 console 输出信息
+            config.optimization.minimizer[0].options.terserOptions.compress.drop_console = true
+
+            // Gzip 压缩
+            const CompressionPlugin = require('compression-webpack-plugin')
+            config.plugins.push(
+                new CompressionPlugin({
+                    algorithm: 'gzip',  //
+                    test: /\.(js|css|woff|woff2|svg)$/, // 哪些文件会被压缩
+                    threshold: 10240, // 对超过10k的数据压缩
+                    deleteOriginalAssets: false, // 不删除压缩前的文件，如果浏览器不支持 Gzip ,则会加载源文件
+                    minRatio: 0.8 // 压缩比大于 0.8 的文件将不会被压缩
+                })
+            )
+
+            // 将 js 文件夹添加时间戳，这样浏览器不会加载上个版本缓存的代码
+            config.output.filename = `js/[name].${timeStamp}.js`
+            config.output.chunkFilename = `js/[name].${timeStamp}.js`
+
+        } else {
+            // 开发环境配置
         }
-    }
+        return {
+            externals: {
+                'vue': 'Vue',
+                'vue-router': 'VueRouter',
+                'axios': 'axios',
+                'element-ui': 'ElementUI',
+                'lottie': 'lottie',
+                'introJs': 'introJs',
+                'highcharts': 'highcharts',
+                'highcharts-vue': 'highcharts-vue'
+            }
+        }
+    },
 }
 
