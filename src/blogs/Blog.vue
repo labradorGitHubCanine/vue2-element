232ebe8ffa6c41afaed3b44b9c8e2a0206476909
labradoGithubCanine <123349527@qq.com>
2021-03-15 22:45:28 +0800

    0

--- src/blogs/Blog.vue
+++ src/blogs/Blog.vue
@@ -3,12 +3,23 @@
         <el-card>
             <el-row>
                 <el-col style="padding: 8px" v-for="i in 10" :key="i" :span="6">
-                    <el-card shadow="hover"></el-card>
+                    <el-card shadow="hover" :body-style="{padding: 0}">
+                        <lottie :animation-data="path"/>
+                    </el-card>
                 </el-col>
             </el-row>
         </el-card>
     </el-main>
 </template>
 <script>
-    export default {}
+    import Lottie from "@/components/Lottie"
+
+    export default {
+        components: {Lottie},
+        data() {
+            return {
+                path: require("@/assets/json/blog.json"),
+            }
+        }
+    }
 </script>
