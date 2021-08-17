55ca6f4accb97c49897afc5c58c2d544814cb90e
labradorGitHubCanine <123349527@qq.com>
2021-08-17 14:03:03 +0800

    0

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -1,25 +1,25 @@
 <template>
-    <el-container>
-        <aside-menu/>
-        <el-container class="is-vertical">
-            <header-bar/>
-            <el-scrollbar>
-                <el-main>
-                    <keep-alive>
-                        <router-view style="min-height: 36rem"/>
-                    </keep-alive>
-                    <footer-bar/>
-                </el-main>
-            </el-scrollbar>
-        </el-container>
+  <el-container>
+    <aside-menu/>
+    <el-container class="is-vertical">
+      <header-bar/>
+      <el-scrollbar>
+        <el-main>
+          <keep-alive>
+            <router-view style="min-height: 36rem"/>
+          </keep-alive>
+          <footer-bar/>
+        </el-main>
+      </el-scrollbar>
     </el-container>
+  </el-container>
 </template>
 <script>
-    import AsideMenu from "@/components/layout/AsideMenu";
-    import FooterBar from "@/components/layout/FooterBar";
-    import HeaderBar from "@/components/layout/HeaderBar";
+import AsideMenu from "@/components/layout/AsideMenu";
+import FooterBar from "@/components/layout/FooterBar";
+import HeaderBar from "@/components/layout/HeaderBar";
 
-    export default {
-        components: {AsideMenu, FooterBar, HeaderBar},
-    }
+export default {
+  components: {AsideMenu, FooterBar, HeaderBar},
+}
 </script>
